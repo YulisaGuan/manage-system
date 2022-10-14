39cd068a42097c785aac2b38d71e5eea0ee7dccf
yulisa <757328207@qq.com>
2022-10-14 18:54:53 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Manage System"/>
+  <HelloWorld msg="Welcome to FS Manage System"/>
 </template>
 
 <script>
